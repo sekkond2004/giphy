f0313a8b7801d49c2fb09b49fd53051fdcfc9b41
sekkond2004 <nguyen.ekko@ic.edu>
2025-11-02 20:21:22 -0600

    end point added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
 console.log("script.js loaded");
+let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=47kgaWAe7OPAUYv6TmU3XEJwYTzGHEy1&q=yasuo&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
