1706ede07c9153afcf65192a14640926b6461a71
sekkond2004 <nguyen.ekko@ic.edu>
2025-11-02 20:46:54 -0600

    enable fetch gif button and fix fetch link

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 console.log("script.js loaded");
 let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=47kgaWAe7OPAUYv6TmU3XEJwYTzGHEy1&q=yasuo&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
 async function getGif() {
-    const response = await fetch("https://api.giphy.com/v1/gifs/search?");
+    const response = await fetch("https://api.giphy.com/v1/gifs/search");
     const data = await response.json();
     const gif = data.gifs[0];
 
@@ -11,9 +11,10 @@ async function getGif() {
 
 getGif()
 
-const button = document.getElementById("gif-container");
+const button = document.getElementById("fetch-gif-btn");
 
 button.addEventListener("click", function() {
     getGif();
 });
+console.log();
 let images = [];
